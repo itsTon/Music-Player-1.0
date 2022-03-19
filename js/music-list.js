d50116ab21627b50f506ecd70c405d235029544f
Arup Mandal <40239193+arupmandal@users.noreply.github.com>
2022-03-20 04:03:17 +0530

    nit commit

---
+++ js/music-list.js
@@ -0,0 +1,55 @@
+
+
+let allMusic = [
+    {
+      name: "Mohni",
+      artist: "Monika Verm",
+      img: "1",
+      src: "1"
+    },
+    {
+      name: "Baarish - Yaariyan ",
+      artist: "Mohammed Irfan,",
+      img: "3",
+      src: "3"
+    },
+    {
+      name: "Tumse Pyaar Karke",
+      artist: "Jubin Nautiyal, Payal Dev",
+      img: "2",
+      src: "2"
+    },
+    {
+      name: "Arziyaanm",
+      artist: "Vikrant Bhartiya, Aishwarya Majmudar",
+      img: "4",
+      src: "4"
+    },
+    {
+      name: "Tum Hi Ho - Aashiqui 2",
+      artist: "Arijit Singh",
+      img: "5",
+      src: "5"
+    },
+    {
+      name: "Khuda Jaane (Bachna Ae Haseeno)",
+      artist: "K.K",
+      img: "6",
+      src: "6"
+    },
+    {
+      name: "O Re Piya",
+      artist: "Rahat Fateh Ali Khan",
+      img: "7",
+      src: "7"
+    },
+    {
+      name: "Ishq Sufiyana - The Dirty Picture",
+      artist: "Vishal-Shekhar, Kamal Khan",
+      img: "8",
+      src: "8"
+    },
+    
+    
+    
+  ];
